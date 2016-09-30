43260a8495f5c6582d46638bcd2eea9994a54efc
Karthik macbook pro <mkarthik415@gmail.com>
2016-09-30 17:02:11 -0500

    made some changes to validations

--- src/app/form/formComponent.ts
+++ src/app/form/formComponent.ts
@@ -1,8 +1,9 @@
 import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators, Validator} from "@angular/forms";
-import { FormControl } from '@angular/forms';
-import {Injectable} from '@angular/core';
-// import { emailValidator, matchingPasswords } from './validators';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {
+    emailValidator, matchingPasswords, passwordValidator, smallValidator
+} from './validators';
+import {Headers, URLSearchParams,Http, Response} from "@angular/http";
 
 
 
@@ -18,107 +19,57 @@ import {Injectable} from '@angular/core';
 export class FormComponent
 {
 
-    // registerForm: FormGroup;
-    //
-    // constructor(private formBuilder: FormBuilder) {
+    registerForm: FormGroup;
+    httpClient:Http;
+
+    constructor(private formBuilder: FormBuilder, private http:Http) {
+        this.httpClient = http;
+
+        this.registerForm = formBuilder.group({
+                companyName: ['', Validators.required],
+                country: ['', Validators.required],
+                url: ['', Validators.required],
+                companyProfile: ['', Validators.required],
+                employees: ['', Validators.required],
+                describe: ['', Validators.required],
+                workEmail: ['', Validators.compose([Validators.required,emailValidator])],
+                accManager: ['', Validators.required],
+                password: ['', Validators.compose([Validators.required, smallValidator])],
+                repPassword: ['', Validators.required],
+                phone: ['', Validators.required]
+            }
+            , {validator: matchingPasswords('password','repPassword')}
+        );
+            // {Validator: areEqual})
+
+    }
+
+    ngOnInit() {
 
 
-    //     this.registerForm = formBuilder.group({
-    //             companyName: ['', Validators.required],
-    //             country: ['', Validators.required],
-    //             url: ['', Validators.required],
-    //             companyProfile: ['', Validators.required],
-    //             employees: ['', Validators.required],
-    //             describe: ['', Validators.required],
-    //             workEmail: ['', Validators.compose([Validators.required, emailValidator])],
-    //             accManager: ['', Validators.required],
-    //             password: ['', Validators.required],
-    //             repPassword: ['', Validators.required],
-    //             phone: ['', Validators.required]
-    //         }, {validator: matchingPasswords(this.formBuilder.group('password'))})
-    //         // {Validator: areEqual})
-    //
-    // }
+    }
 
-    // ngOnInit() {
-
-        // function validateEmail() {
-        //     return (control: FormControl): {[key: string]: any} => {
-        //         let EMAIL_REGEXP = /^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i;
-        //
-        //         if (control.value && !EMAIL_REGEXP.test(control.value)) {
-        //             return {invalidEmail: true};
-        //         }
-        //     }
-        // }
+        submitRegistration(value: any)
+            {
+                console.log(value);
+                var headers = new Headers();
+                var accManager=value.controls.accManager.value;
+                var password=value.controls.repPassword.value;
+                headers.append('Access-Control-Allow-Origin', '*');
+                headers.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
 
 
-        // function matchingPasswords(control: FormControl): {[key: string]: any} {
-        //
-        //     // let password = group[passwordKey];
-        //     // let confirmPassword = group.value;
-        //
-        //     if (control.value !== control.root.toString()) {
-        //         return {
-        //             mismatchedPasswords: true
-        //         };
-        //     }
-        //  }
+                this.httpClient.get('http://localhost:8080/login?username='+accManager+'&password='+password, {
+                    headers: headers
+                })
+                    .map(res => res.text())
+                    .subscribe(data => console.log(data),
+                        err => console.log(err),
+                        () => console.log('Secret Quote Complete'));
+                console.log(value.controls.accManager.value);
 
 
+            }
 
 
-    //     this.registerForm = this.formBuilder.group({
-    //         companyName: ['', Validators.required],
-    //         country: ['', Validators.required],
-    //         url: ['', Validators.required],
-    //         companyProfile: ['', Validators.required],
-    //         employees: ['', Validators.required],
-    //         describe: ['', Validators.required],
-    //         workEmail: ['', Validators.compose([Validators.required, validateEmail])],
-    //         accManager: ['', Validators.required],
-    //         password: ['', Validators.required],
-    //         repPassword: ['', Validators.required],
-    //         phone: ['', Validators.required]
-    //     }
-    //     );
-    //
-    //
-    // }
-
-        // submitRegistration(value: any)
-        //     {
-        //         console.log(value);
-                // var headers = new Headers();
-                // var responseData;
-                // headers.append('Access-Control-Allow-Origin', '*');
-                // headers.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-
-
-                // this.http.get('http://localhost:8080/echo?name=karthik', {
-                //     headers: headers
-                // })
-                //     .map(res => res.text())
-                //     .subscribe(data => responseData = data,
-                //         err => console.log(err),
-                //         () => console.log('Secret Quote Complete'));
-                // console.log(value);
-
-
-
-                // let wikiUrl = 'http://en.wikipedia.org/w/api.php';
-                // let params = new URLSearchParams();
-                // params.set('search', 'angularjs'); // the user's search value
-                // params.set('action', 'opensearch');
-                // params.set('format', 'json');
-                // params.set('callback', 'JSONP_CALLBACK');
-                // // TODO: Add error handling
-                // return this.jsonp
-                //     .get(wikiUrl, { search: params })
-                //     .map(response => <string[]> response.json()[1]);
-
-
-            // }
-
-        //
 }
--- src/app/form/validators.ts
+++ src/app/form/validators.ts
@@ -7,28 +7,96 @@
  */
 
 // SINGLE FIELD VALIDATORS
-// import {FormControl} from "@angular/forms";
-// import {ControlGroup} from "@angular/common";
+import {FormControl,FormGroup,FormBuilder} from "@angular/forms";
+import {ControlGroup} from "@angular/common";
+
+
+export function emailValidator(control: FormControl): {[key: string]: any} {
+    let EMAIL_REGEXP = /^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i;
+
+    if (control.value && !EMAIL_REGEXP.test(control.value)) {
+        return {invalidEmail: true};
+    }
+}
+
+//CONTROL GROUP VALIDATORS
+export function matchingPasswords(passwordKey: string, confirmPasswordKey: string) {
+    return (group: ControlGroup): {[key: string]: any} => {
+        let password = group.controls[passwordKey];
+        let confirmPassword = group.controls[confirmPasswordKey];
+
+        if (password.value !== confirmPassword.value) {
+            return {mismatchedPasswords: true};
+        }
+    }
+}
+
+// password requirements
+
+// export function passwordValidator(control: FormControl): {[key: string]: any} {
 //
+//     // let PASSWORD_REGEXP =/^(.{0,7}|[^0-9]*|[^A-Z]*|[a-zA-Z0-9]*)$/;
+//     let PASSWORD_REGEXP =/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,}/;
 //
-// export function emailValidator(control: FormControl): {[key: string]: any} {
-//     let EMAIL_REGEXP = /^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i;
-//
-//     if (control.value && !EMAIL_REGEXP.test(control.value)) {
-//         return {invalidEmail: true};
+//     if (control.value && !PASSWORD_REGEXP.test(control.value)) {
+//         return {invalidPassword: true};
 //     }
 // }
 
+export function passwordValidator(control: FormControl): {[key: string]: any} {
 
-//CONTROL GROUP VALIDATORS
-// export function matchingPasswords(group: ControlGroup): {[key: string]: any} => {
-//         let password = group.controls['password'];
-//         let confirmPassword = group.controls['repPassword'];
+    // let PASSWORD_REGEXP =/^(.{0,7}|[^0-9]*|[^A-Z]*|[a-zA-Z0-9]*)$/;
+    let PASSWORD_REGEXP =/^(?=.*[A-Z])/;
+
+    if (control.value && !PASSWORD_REGEXP.test(control.value)) {
+        return {invalidPassword: true};
+    }
+}
+
+export function smallValidator(control: FormControl): {[key: string]: any} {
+
+    // let PASSWORD_REGEXP =/^(.{0,7}|[^0-9]*|[^A-Z]*|[a-zA-Z0-9]*)$/;
+    let SMALL_REGEXP =/(.*[a-z])/;
+    let CAP_REGEXP =/(.*[A-Z])/;
+
+    if(control.value && CAP_REGEXP.test(control.value) && SMALL_REGEXP.test(control.value)) {
+        return  {capPasswordError: true,smallPasswordError: true};
+    }
+    else if(control.value && CAP_REGEXP.test(control.value)) {
+        return {capPasswordError: true};
+    }
+    else if(control.value && SMALL_REGEXP.test(control.value)) {
+        return {smallPasswordError: true};
+    }
+}
+
+
+// export function smallValidator(control: FormControl): {[key: string]: any} {
 //
-//         if (password.value !== confirmPassword.value) {
-//             return {
-//                 mismatchedPasswords: true
-//             };
-//         }
+//     // let PASSWORD_REGEXP =/^(.{0,7}|[^0-9]*|[^A-Z]*|[a-zA-Z0-9]*)$/;
+//     let SMALL_REGEXP =/^(?=.*[0-9])/;
+//
+//     if (control.value && !SMALL_REGEXP.test(control.value)) {
+//         return {smallPassword: true};
+//     }
 // }
-
+//
+// export function smallValidator(control: FormControl): {[key: string]: any} {
+//
+//     // let PASSWORD_REGEXP =/^(.{0,7}|[^0-9]*|[^A-Z]*|[a-zA-Z0-9]*)$/;
+//     let SMALL_REGEXP =/^(?=.*[!@#\$%\^&\*])/;
+//
+//     if (control.value && !SMALL_REGEXP.test(control.value)) {
+//         return {smallPassword: true};
+//     }
+// }
+//
+// export function smallValidator(control: FormControl): {[key: string]: any} {
+//
+//     // let PASSWORD_REGEXP =/^(.{0,7}|[^0-9]*|[^A-Z]*|[a-zA-Z0-9]*)$/;
+//     let SMALL_REGEXP =/^(?=.{8,})/;
+//
+//     if (control.value && !SMALL_REGEXP.test(control.value)) {
+//         return {smallPassword: true};
+//     }
+// }
