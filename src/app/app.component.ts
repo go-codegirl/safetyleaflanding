34ac6376da7509f1fd6a1fc7cb2c6c9116d6984d
Karthik macbook pro <mkarthik415@gmail.com>
2016-10-13 20:11:32 -0500

    added freshchat and fixed ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import {Component} from '@angular/core';
 @Component({
     selector: 'my-app',
     templateUrl: './app/app.component.html',
-    styleUrls: ['app/css/indexPage.css', 'app/css/bootstrap.min.css']
+    styleUrls: ['src/app/css/indexPage.css', 'src/app/css/bootstrap.min.css']
 })
 
 export class AppComponent {
